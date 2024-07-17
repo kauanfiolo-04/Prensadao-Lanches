dead21ef9d3610c33580b6c554b75dd427f5c142
Kauan Fiolo da Silva <kauanfiolo@hotmail.com>
2024-07-17 17:18:18 -0300

    add express intial server, remove eslint because it's making some conflicts with any package i wanna install

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log('TS tomaaa')
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const app= express()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req,res)=>{
+  res.send('hello world')
+})
+
+app.listen(port,()=>console.log(`listen in: http://localhost/${port}`))
