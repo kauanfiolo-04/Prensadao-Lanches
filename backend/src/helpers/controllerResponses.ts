dc0683000b935eb9630d4d488e8888657d748262
Kauan Fiolo da Silva <kauanfiolo@hotmail.com>
2024-07-23 08:44:56 -0300

    refactor add HttpStatusCode Enum

--- backend/src/helpers/controllerResponses.ts
+++ backend/src/helpers/controllerResponses.ts
@@ -1,9 +1,9 @@
-import { HttpResponse } from "../controllers/protocols"
+import { HttpResponse, HttpStatusCode } from "../controllers/protocols"
 
-export const ok = <T>(body: any):HttpResponse<T> => ({statusCode:200, body})
+export const ok = <T>(body: any):HttpResponse<T> => ({statusCode:HttpStatusCode.OK, body})
 
-export const created = <T>(body: any):HttpResponse<T> => ({statusCode:201, body})
+export const created = <T>(body: any):HttpResponse<T> => ({statusCode:HttpStatusCode.CREATED, body})
 
-export const badRequest = (message:string):HttpResponse<string> => ({statusCode: 400, body: message})
+export const badRequest = (message:string):HttpResponse<string> => ({statusCode: HttpStatusCode.BAD_REQUEST, body: message})
 
-export const serverError = ():HttpResponse<string> => ({statusCode:500, body:'Something went wrong'})
+export const serverError = ():HttpResponse<string> => ({statusCode:HttpStatusCode.SERVER_ERROR, body:'Something went wrong'})
